780fb94f6a656037eeb0cff8a8fcd1f0e74a26b3
kindfi <colorz55555@gmail.com>
2021-10-04 18:26:52 +0300

    L01

---
+++ MihaiFlorentin/L01/Exemple/Exemple/Domain/Quantity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemple.Domain
+{
+    public record Quantity
+    {
+        public decimal Value { get; }
+
+        public Quantity(decimal value)
+        {
+            if (value > 0 && value <= 100)
+            {
+                Value = value;
+            }
+            else
+            {
+                throw new InvalidQuantityException($"{value:0.##} is an invalid grade value.");
+            }
+        }
+
+        public Quantity Round()
+        {
+            var roundedValue = Math.Round(Value);
+            return new Quantity(roundedValue);
+        }
+
+        public override string ToString()
+        {
+            return $"{Value:0.##}";
+        }
+    }
+}
---
+++ MihaiFlorentin/L01/Exemple/Exemple/Program.cs
@@ -0,0 +1,64 @@
+﻿using Exemple.Domain;
+using System;
+using System.Collections.Generic;
+using static Exemple.Domain.Cart;
+
+namespace Exemple
+{
+    class Program
+    {
+        private static readonly Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            var listOfProducts = ReadlistOfProducts().ToArray();
+            UnvalidatedCart unvalidatedProducts = new(listOfProducts);
+            ICart result = ValidateCart(unvalidatedProducts);
+            result.Match(
+                whenUnvalidatedCart: unvalidatedResult => unvalidatedProducts,
+                whenPaidCart: PaidResult => PaidResult,
+                whenEmptyCart: emptyResult => emptyResult,
+                whenValidatedCart: validatedResult => PaidCart(validatedResult)
+            );
+
+            Console.WriteLine("Hello World!");
+        }
+
+        private static List<UnvalidatedShoppingCart> ReadlistOfProducts()
+        {
+            List <UnvalidatedShoppingCart> listOfProducts = new();
+            do
+            {
+                //read registration number and quantity and create a list of greads
+                var registrationNumber = ReadValue("Registration Number: ");
+                if (string.IsNullOrEmpty(registrationNumber))
+                {
+                    break;
+                }
+
+                var quantity = ReadValue("Quantity: ");
+                if (string.IsNullOrEmpty(quantity))
+                {
+                    break;
+                }
+
+                listOfProducts.Add(new (registrationNumber, quantity));
+            } while (true);
+            return listOfProducts;
+        }
+
+        private static ICart ValidateCart(UnvalidatedCart unvalidatedProducts) =>
+            random.Next(100) > 50 ?
+            new EmptyCart(new List<UnvalidatedShoppingCart>(), "Random errror")
+            : new ValidatedCart(new List<ValidatedShoppingCart>());
+
+        private static ICart PaidCart(ValidatedCart validCart) =>
+            new PaidCart(new List<ValidatedShoppingCart>(), DateTime.Now);
+
+        private static string? ReadValue(string prompt)
+        {
+            Console.Write(prompt);
+            return Console.ReadLine();
+        }
+    }
+}
